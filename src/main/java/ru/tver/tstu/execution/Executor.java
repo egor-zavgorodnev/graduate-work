2e0c7425a244003f0b58d1262ca21a03f5738220
Egor <ze99@rambler.ru>
2021-02-14 14:27:06 +0300

    add opened cmd output

--- src/main/java/ru/tver/tstu/execution/Executor.java
+++ src/main/java/ru/tver/tstu/execution/Executor.java
@@ -1,44 +1,27 @@
 package ru.tver.tstu.execution;
 
-import ru.tver.tstu.backend.lexems.*;
-import ru.tver.tstu.backend.model.*;
-import ru.tver.tstu.backend.syntax.*;
-
 import java.io.*;
-import java.nio.file.*;
-import java.util.*;
+
 
 public class Executor {
 
-    private static final String CLASS_FILE_PATH = "file.class"; //root dir;
+    //root dir;
 
-
-/*    public static void main(String[] args) throws IOException, IllegalAccessException, InstantiationException {
-        LexicalAnalyzer lexicalAnalyzer = new LexicalAnalyzer();
-
-        IdentifierTable nameTable = new IdentifierTable();
-        List<Keyword> lexems = lexicalAnalyzer.recognizeAllLexem(args[0]);
-        nameTable.recognizeAllIdentifiers(lexems);
-
-        RecursiveDescentParser syntaxAnalyzer = new SyntaxParserWithBytecodeGen(lexems, nameTable);
-        syntaxAnalyzer.checkSyntax();
-
-        Class<?> aClass = ByteCodeLoader.clazz.loadClass(Files.readAllBytes(Path.of(CLASS_FILE_PATH)));
-
-        ((Runnable) aClass.newInstance()).run();
-    }*/
     public static void execute(String sourceCode) throws IllegalAccessException, InstantiationException, IOException {
-        LexicalAnalyzer lexicalAnalyzer = new LexicalAnalyzer();
 
-        IdentifierTable nameTable = new IdentifierTable();
-        List<Keyword> lexems = lexicalAnalyzer.recognizeAllLexem(sourceCode);
-        nameTable.recognizeAllIdentifiers(lexems);
+        File startBat = new File("start.bat");
+        FileWriter fileWriter = new FileWriter(startBat);
 
-        RecursiveDescentParser syntaxAnalyzer = new SyntaxParserWithBytecodeGen(lexems, nameTable);
-        syntaxAnalyzer.checkSyntax();
+        fileWriter.write("@echo off\n");
+        fileWriter.write("java -jar executor.jar " + sourceCode.replaceAll("\n", "/n"));
+        fileWriter.write("\npause");
+        fileWriter.close();
 
-        Class<?> aClass = ByteCodeLoader.clazz.loadClass(Files.readAllBytes(Path.of(CLASS_FILE_PATH)));
-
-        ((Runnable) aClass.newInstance()).run();
+        Runtime rt = Runtime.getRuntime();
+        try {
+            rt.exec(new String[]{"cmd.exe","/c","start start.bat"});
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ start.bat
@@ -0,0 +1,3 @@
+@echo off
+java -jar executor.jar dfg
+pause

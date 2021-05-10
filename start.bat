7a402b7da58974012920f8afeea67376bac6c683
Egor <ze99@rambler.ru>
2021-05-10 15:56:27 +0300

    execute button bug fix

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-java -jar executor.jar "Var x, squ;/n/nProcedure square;/n Begin/n   squ:= x * x/n End;/n/n Begin/n   x := 1;/n   While x < 10 Do/n   Begin/n      Call square;/n      x := x + 1/n   End/n End./n"
+java -jar executor.jar "Var x, squ;/n/nProcedure rec;/n Begin/n   x := x - 1;/n   If x > 0 Then/n   Begin/n      Call rec/n   End/n End;/n/n Begin/n   x := 9;/n   Call rec/n End./n"
 pause
 exit

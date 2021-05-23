11e1867579585e465b3316a81a7b8f8903b0eb74
Egor <ze99@rambler.ru>
2021-05-23 15:08:05 +0300

    add full example

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-java -jar executor.jar "Var x, squ;/n/nProcedure square;/n Begin/n   squ:= x * x/n End;/n/n Begin/n   x := 1;/n   While x < 10 Do/n   Begin/n      Call square;/n      x := x + 1/n   End/n End./n"
+java -jar executor.jar "var x, squ, cub;/n/nprocedure square;/n begin/n   squ:= x * x/n End;/n/n Procedure cube;/n  Begin/n    cub:= x * x * x/n  End;/n/n Begin/n   x := 1;/n   While x < 10 Do/n   Begin/n       If Odd x Then/n       Begin/n          Call square;/n          Call cube/n       End;/n       x := x + 1/n   End/n End./n"
 pause
 exit

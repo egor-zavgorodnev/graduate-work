a2733c4211e5bfa10a8f1dad750fa651c58bd9e9
Egor <ze99@rambler.ru>
2021-05-23 12:52:49 +0300

    remove useless code

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-java -jar executor.jar " var n,f;/n begin/n    f := 2;/n    n := 2;/n    n := f + 1/n end./n"
+java -jar executor.jar "Var x, squ;/n/nProcedure square;/n Begin/n   squ:= x * x/n End;/n/n Begin/n   x := 1;/n   While x < 10 Do/n   Begin/n      Call square;/n      x := x + 1/n   End/n End./n"
 pause
 exit
